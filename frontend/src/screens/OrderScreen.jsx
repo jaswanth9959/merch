e6b696632d39dff0d00137c66584a45d20a6eb1c
jaswanth9959 <jayreddy731@gmail.com>
2024-04-06 00:41:24 -0500

    final code

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -214,7 +214,7 @@ const OrderScreen = () => {
                   <Col>${order.totalPrice}</Col>
                 </Row>
               </ListGroup.Item>
-              {!order.isPaid && (
+              {!order.isPaid && !userInfo.isAdmin && (
                 <ListGroup.Item>
                   {loadingPay && <Loader />}
 
